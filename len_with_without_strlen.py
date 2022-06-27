3ed0bc8abcef71c7d0662215e0985f789ce00907
Darshan Bashetti <104633319+Darshanbashetti@users.noreply.github.com>
2022-06-27 19:46:13 +0530

    Add files via upload

---
+++ len_with_without_strlen.py
@@ -0,0 +1,11 @@
+# with length function
+str = input("enter a string :")
+print("length of ", str, "is", len(str))
+
+# wihout using length function
+str1 = input("enter a string :")
+count = 0
+
+for i in str1:
+    count += 1
+print(count)
