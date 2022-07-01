974697c586410d520505522f865f55bb435ece0d
Darshan Bashetti <104633319+Darshanbashetti@users.noreply.github.com>
2022-07-01 19:25:45 +0530

    Add files via upload

---
+++ Area_of_circle_rect_tri.py
@@ -0,0 +1,10 @@
+
+length = int(input("Enter length :"))
+breadth = int(input("Enter breadth :"))
+radius = int(input("enter radius :"))
+base = int(input("Enter base :"))
+height = int(input("enter height :"))
+
+print("Area of circle is", 3.142*radius*radius)
+print("Area of rectangle is", length*breadth)
+print("Area of triangle is", base*height)
