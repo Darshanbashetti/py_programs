c8c46b0048fbd27ed04a805e3e3d11862e6fa4cf
Darshan Bashetti <104633319+Darshanbashetti@users.noreply.github.com>
2022-07-02 19:23:18 +0530

    Add files via upload

---
+++ Power_Of_Num.py
@@ -0,0 +1,11 @@
+num = int(input("Enter a number  : "))
+power = int(input("Enter a power : "))
+result = num ** power
+print(result)
+
+
+# using pow() function
+
+num = int(input("Enter a number  : "))
+power = int(input("Enter a power : "))
+print(pow(num, power))
