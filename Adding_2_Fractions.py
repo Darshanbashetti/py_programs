8667b6f8f61a8456150b3a7365e951e05c0612b4
Darshan Bashetti <104633319+Darshanbashetti@users.noreply.github.com>
2022-07-02 19:20:26 +0530

    Add files via upload

---
+++ Adding_2_Fractions.py
@@ -0,0 +1,12 @@
+import fractions
+
+
+import fractions
+num1 = int(input("Enter first numaretor :"))
+num2 = int(input("Enter second denomenator :"))
+num3 = int(input("Enter first numaretor :"))
+num4 = int(input("Enter second numaretor :"))
+
+f1 = fractions.Fraction(num1, num2)
+f2 = fractions.Fraction(num3, num4)
+print(f1+f2)
