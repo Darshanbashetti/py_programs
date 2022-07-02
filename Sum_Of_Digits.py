d11803ca3a9ee4373be1d371cfeff0350fb2d794
Darshan Bashetti <104633319+Darshanbashetti@users.noreply.github.com>
2022-07-02 19:24:22 +0530

    Add files via upload

---
+++ Sum_Of_Digits.py
@@ -0,0 +1,15 @@
+n = int(input("Enter a number :"))
+s = str(n)
+sum = 0
+for i in s:
+    sum += int(i)
+print(sum)
+
+
+# multiplication
+n1 = int(input("Enter a number :"))
+s1 = str(n1)
+mul = 1
+for i in s1:
+    mul *= int(i)
+print(mul)
