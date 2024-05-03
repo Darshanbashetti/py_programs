13f84b5a8a669f9e556a9c52587b53c3ec668e7d
Darshan Bashetti <104633319+Darshanbashetti@users.noreply.github.com>
2024-05-03 18:52:03 +0530

    Add files via upload

--- replace.py
+++ replace.py
@@ -1,20 +1,37 @@
-# n=int(input())
+# # n=int(input())
 
-# for i in range(1,n):
-#     if n%i==0:
-#         print(i)
+# # for i in range(1,n):
+# #     if n%i==0:
+# #         print(i)
         
     
-# replace all xeros into 1
-# and 1 into 2
+# # replace all xeros into 1
+# # and 1 into 2
 
-a='1781010101010'
-b=''
-for i in a:
-    if i=='0':
-        b=b+'1'
-    elif i=='1':
-        b=b+'2'
-    else:
-        b=b+i
-print(b)
+# a='1781010101010'
+# b=''
+# for i in a:
+#     if i=='0':
+#         b=b+'1'
+#     elif i=='1':
+#         b=b+'2'
+#     else:
+#         b=b+i
+# print(b)
+
+# a='dfbsdbsdfbb'
+# b=''
+# for i in a:
+#     if i not in b:
+#         b=b+i
+# print(b)
+
+# a='dfbsdbsdfbb'
+# b=''
+# for i in a:
+#     if i in b:
+#         continue
+#     else:
+#         b=b+i
+# print(b)
+
