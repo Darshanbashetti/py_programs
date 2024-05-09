5d83b9107379ff8b98ede5f3eec7f0cbe3b62d60
Darshan Bashetti <104633319+Darshanbashetti@users.noreply.github.com>
2024-05-09 22:19:06 +0530

    Add files via upload

---
+++ unique.py
@@ -0,0 +1,32 @@
+# # n=[2,4,6,7,4,2,1,8,2,1,4,9]
+# # m=0
+# # for i in n:
+# #     for j in range(1,len(n)):
+# #         if (i>n[j]):
+# #             if i>m:
+# #                 m=i
+# # n.remove(m)
+
+# # print(max(n))
+
+# n=[1,2,0,2,2,0,0,0,0,0,1,2,0,1,1,0]
+# m=[]
+# for i in n:
+#     if i==0:
+#         m.append(i)
+# for i in n:
+#     if i==1:
+#         m.append(i)
+# for i in n:
+#     if i==2:
+#         m.append(i)
+# print(m)
+
+a='i am participating in an interview'
+b=''
+for i in a:
+    b=b+i
+    if i==" ":
+        b=" "
+print(b)
+
